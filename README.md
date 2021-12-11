6df82654c18e3bbe9560aca3294ae145b79b7463
Schr0dingerCat <174683306@qq.com>
2021-12-11 11:16:36 +0800

    完全使用setup，注意学习使用 模板 refs 用法

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ backend use go + gin, frontend use vue3 + element-plus + axios
 Q1、后端需要定义与前端对应的结构体，前端setup中使用ref()定义const常量对象a，需要使用a.value进行赋值
 Q2、使用 el-select option 获取的数据量大时，会有卡顿
 A2、之后使用 el-select-v2，默认的v2列表框显示文字位置不对
-Q3、前端使用setup导出 form，form.xxx 无法取到select 选择的值
+Q3、前端使用setup导出 form，<template>中使用 ref="form"，form.xxx 无法取到select 选择的值，具体需要查看 template Refs的文档
--- frontend/src/components/Erptools.vue
+++ frontend/src/components/Erptools.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-form ref="form" :model="form" label-width="120px">
+  <el-form :model="form" label-width="120px">
     <el-form-item label="订单预交日">
       <el-date-picker
         v-model="form.date1"
@@ -25,57 +25,49 @@
 </template>
 
 <script lang="ts">
-import { ref, reactive, defineComponent } from 'vue'
-import axios from "axios"
+import { ref, reactive, defineComponent } from "vue";
+import axios from "axios";
 export default defineComponent({
-  data() {
-    return {
-      form: {
-        prdno: '',
-        date1: '',
-      },
-    }
-  },
   setup() {
     // 定义常量对象，使用时需要用 xx.value
     const options = ref([
       {
-        value: '',
-        label: ''
-      }
-    ])
+        value: "",
+        label: "",
+      },
+    ]);
 
     // 加载 /erptools界面是自动获取数据
-    axios.post('/erptools', {
-      cmd: 'getprdno'
-    })
+    axios
+      .post("/erptools", {
+        cmd: "getprdno",
+      })
       .then(function (response) {
-        options.value = response.data.options
+        options.value = response.data.options;
       })
       .catch(function (error) {
-        console.log(error)
-      })
+        console.log(error);
+      });
 
     // TODO: 发送数据，后台查询
     const onSubmit = () => {
-      console.log('submit!')
-      axios.post('/erptools', {
-        cmd: 'getsolist'
-      })
-        .then((response) => {
-
-        }
-        )
-        .catch((error) => {
-
-        }
-        )
-    }
+      console.log("submit!");
+      axios
+        .post("/erptools", {
+          cmd: "getsolist",
+        })
+        .then((response) => {})
+        .catch((error) => {});
+    };
 
     return {
+      form: ref({
+        prdno: "",
+        date1: "",
+      }),
       options,
-      onSubmit
-    }
-  }
-})
+      onSubmit,
+    };
+  },
+});
 </script>
